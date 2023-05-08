77fd8e50393b605d38414dfb07a17b8b3d2cab05
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2023-05-07 23:20:44 -0700

    better integrate with Netlify

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,7 +1,27 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import netlify from '@astrojs/netlify/functions';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+const adapters = {
+  netlify: netlify(),
+  vercel: vercel()
+}
+
 export default defineConfig({
-    integrations: [svelte()],
+  output: 'server',
+  adapter: adapters[process.env.PLATFORM] || netlify(), // Use Netlify as fallback
+  integrations: [svelte()],
+  style: {
+    postcss: {}
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "node-domexception": "/domexception-shim.js"
+      }
+    }
+  }
 });
