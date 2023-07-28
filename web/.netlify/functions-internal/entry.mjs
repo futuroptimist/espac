423a9158aa20d4c0eda4d509d1ea8dcf9b452d82
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2023-07-27 22:04:30 -0700

    refresh esp.ac

--- web/.netlify/functions-internal/entry.mjs
+++ web/.netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
-import { s as server_default, d as deserializeManifest } from './chunks/astro.136fe095.mjs';
-import { _ as _page0 } from './chunks/pages/all.e1ae504a.mjs';
+import { s as server_default, g as deserializeManifest } from './chunks/astro.09442b6c.mjs';
+import { _ as _page0 } from './chunks/pages/all.faa137f4.mjs';
 import 'mime';
 import 'cookie';
 import 'html-escaper';
@@ -31,7 +31,7 @@ const _renderer1 = {
 const pageMap = new Map([["src/pages/index.astro", _page0],]);
 const renderers = [Object.assign({"name":"astro:jsx","serverEntrypoint":"astro/jsx/server.js","jsxImportSource":"astro"}, { ssr: server_default }),Object.assign({"name":"@astrojs/svelte","clientEntrypoint":"@astrojs/svelte/client.js","serverEntrypoint":"@astrojs/svelte/server.js"}, { ssr: _renderer1 }),];
 
-const _manifest = Object.assign(deserializeManifest({"adapterName":"@astrojs/netlify/functions","routes":[{"file":"","links":[],"scripts":[],"styles":[{"type":"external","src":"/_astro/index.74c3a5c2.css"}],"routeData":{"route":"/","type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":false,"_meta":{"trailingSlash":"ignore"}}}],"base":"/","markdown":{"drafts":false,"syntaxHighlight":"shiki","shikiConfig":{"langs":[],"theme":"github-dark","wrap":false},"remarkPlugins":[],"rehypePlugins":[],"remarkRehype":{},"gfm":true,"smartypants":true},"pageMap":null,"componentMetadata":[],"renderers":[],"entryModules":{"\u0000@astrojs-ssr-virtual-entry":"_@astrojs-ssr-virtual-entry.mjs","@astrojs/svelte/client.js":"_astro/client.c4e17359.js","C:/Users/danie/Projects/espac/web/src/components/svelte/Page.svelte":"_astro/Page.14b92237.js","astro:scripts/before-hydration.js":""},"assets":["/_astro/index.74c3a5c2.css","/favicon.svg","/assets/dpsace.svg","/assets/esp.jpg","/_astro/client.c4e17359.js","/_astro/Page.14b92237.js"]}), {
+const _manifest = Object.assign(deserializeManifest({"adapterName":"@astrojs/netlify/functions","routes":[{"file":"","links":[],"scripts":[],"styles":[{"type":"external","src":"/_astro/index.b8e3020c.css"}],"routeData":{"route":"/","type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":false,"_meta":{"trailingSlash":"ignore"}}}],"base":"/","markdown":{"drafts":false,"syntaxHighlight":"shiki","shikiConfig":{"langs":[],"theme":"github-dark","wrap":false},"remarkPlugins":[],"rehypePlugins":[],"remarkRehype":{},"gfm":true,"smartypants":true},"pageMap":null,"componentMetadata":[["C:/Users/danie/Projects/espac/web/src/pages/index.astro",{"propagation":"none","containsHead":true}]],"renderers":[],"entryModules":{"\u0000@astrojs-ssr-virtual-entry":"_@astrojs-ssr-virtual-entry.mjs","@astrojs/svelte/client.js":"_astro/client.c4e17359.js","astro:scripts/before-hydration.js":""},"assets":["/_astro/index.b8e3020c.css","/favicon.svg","/assets/dpsace.svg","/assets/esp.jpg","/_astro/client.c4e17359.js"]}), {
 	pageMap: pageMap,
 	renderers: renderers,
 	
